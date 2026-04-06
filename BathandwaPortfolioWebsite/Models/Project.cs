a5bb89091e4489a6e7cae66920a4da87110bd6f4
Bathandwa <bmap750@gmail.com>
2026-04-06 22:58:29 +0200

    feat: Implemented Project class attributes.

--- BathandwaPortfolioWebsite/Models/Project.cs
+++ BathandwaPortfolioWebsite/Models/Project.cs
@@ -2,5 +2,31 @@ namespace BathandwaPortfolioWebsite.Models;
 
 public class Project
 {
+    public string slug { get; set; }
+    
+    public string title { get; set; }
+    
+    public string group { get; set; }
+    
+    public string techStack { get; set; }
+    
+    public string tagline { get; set; }
+    
+    public string projectStatus { get; set; }
+    
+    public string description { get; set; }
+    
+    public string projectlinkUrl { get; set; }
+    
+    public string liveUrl { get; set; }
+    
+    public string imageUrl { get; set; }
+    
+    public bool featured { get; set; }
+    
+    public string projectRole { get; set; }
+    
+    public string roleDescription { get; set; }
+    
     
 }
