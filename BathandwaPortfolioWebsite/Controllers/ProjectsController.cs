872a3f8ec5a3ced1c990a91802273721852248d8
Bathandwa <bmap750@gmail.com>
2026-05-31 01:54:14 +0200

    !feat: Began implementing ViewBag to display only projects that are featured.

--- BathandwaPortfolioWebsite/Controllers/ProjectsController.cs
+++ BathandwaPortfolioWebsite/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using BathandwaPortfolioWebsite.Models;
 
@@ -7,7 +8,25 @@ namespace BathandwaPortfolioWebsite.Controllers;
 
 public class ProjectsController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly IWebHostEnvironment _env;
+    public ProjectsController(IWebHostEnvironment env)
+    {
+        _env = _env;
+    }
+    public IActionResult Index()
+    {
+        var path = Path.Combine(_env.ContentRootPath, "Data", "projects.json");
+        var json = System.IO.File.ReadAllText(path);
+
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var all = JsonSerializer.Deserialize<List<Project>>(json, options) ?? [];
+
+        var featured = all.Where(p => p.featured == true).ToList();
+
+        ViewBag.featured = featured;
+        
+        return View();  
+    }
     
     public IActionResult Detail(string slug) => View();
 }
