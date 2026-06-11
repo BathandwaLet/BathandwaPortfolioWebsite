9637bc19826d969fcf08006f0b7f1e166359d6d2
Bathandwa <bmap750@gmail.com>
2026-06-11 21:37:53 +0200

    feat: Added function to switch between light and dark mode

--- BathandwaPortfolioWebsite/wwwroot/js/site.js
+++ BathandwaPortfolioWebsite/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-const switcher = document.querySelector('.btn');
+const switcher = document.querySelector('.btn-darkMode');
 
 switcher.addEventListener('click', function(){
     document.body.classList.toggle('light-theme');
