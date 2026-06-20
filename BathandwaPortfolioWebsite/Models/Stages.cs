b627f81665788d0e4d93f3da852dce6c968fa6ef
Bathandwa <bmap750@gmail.com>
2026-06-20 02:08:07 +0200

    feat: Added Stages model to handle project stages for the detail page.

---
+++ BathandwaPortfolioWebsite/Models/Stages.cs
@@ -0,0 +1,20 @@
+namespace BathandwaPortfolioWebsite.Models;
+
+public class Stages
+{
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string Status { get; set; }
+    
+    public string VideoUrl { get; set; }
+    
+    public string[] ImageUrl { get; set; }
+    
+    public string[] ImageDescription { get; set; }
+    
+    public string[] ImageAlt { get; set; }
+    
+    public string GithubUrl { get; set; }
+}
